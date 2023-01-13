85f9012ba8fe7cd598bc84ba976d271fbdd8ded5
TheNefelin <metalflap@gmail.com>
2023-01-12 23:45:24 -0300

    Testing

--- js/data.js
+++ js/data.js
@@ -1,8 +1,8 @@
 const dataLinksMenu = [
     {
         id: 1,
-        nombre: "GitHub",
-        link: "https://github.com/TheNefelin"
+        nombre: "Nada",
+        link: "#"
     }
     ,{
         id: 3,
@@ -11,8 +11,8 @@ const dataLinksMenu = [
     }
     ,{
         id: 3,
-        nombre: "Cachureando",
-        link: "https://slifer.bsite.net/"
+        nombre: "Position",
+        link: "https://www.youtube.com/watch?v=eNGa5_yxmUw"
     }
     ,{
         id: 4,
@@ -21,3 +21,55 @@ const dataLinksMenu = [
     }
 ];
 
+const dataLinks = [
+    {
+        id: 1,
+        nombre: "Azure DevObs @metalflap",
+        link: "https://dev.azure.com/nefelin/dotNETCore6/_build"
+    }
+    ,{
+        id: 2,
+        nombre: "Visual Studio Get Help",
+        link: "https://my.visualstudio.com/GetHelp?mkt=en-us"
+    }
+    ,{
+        id: 3,
+        nombre: "Welcome to Dev Essentials",
+        link: "https://my.visualstudio.com/?auth_redirect=true"
+    }
+    ,{
+        id: 4,
+        nombre: "Freeasp Hosting",
+        link: "https://freeasphosting.net/"
+    }
+    ,{
+        id: 5,
+        nombre: "API Metalflap",
+        link: "https://bsite.net/metalflap/index.htm"
+    }
+    ,{
+        id: 6,
+        nombre: "API Slifer",
+        link: "https://slifer.bsite.net/"
+    }
+    ,{
+        id: 7,
+        nombre: "Scrimba",
+        link: "https://scrimba.com/learn/learnreact"
+    }
+    ,{
+        id: 8,
+        nombre: "Curso React",
+        link: "https://www.youtube.com/watch?v=rLoWMU4L_qE"
+    }
+    ,{
+        id: 9,
+        nombre: "GitHub",
+        link: "https://github.com/TheNefelin"
+    },
+    ,{
+        id: 10,
+        nombre: "Awesome WebSite",
+        link: "https://jacekjeznach.com/"
+    }
+];
